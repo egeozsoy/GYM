46cb1cab2c74edd861652104934e0bc8f2c7743b
egeozsoy <egeozsoy97@gmail.com>
2018-07-06 12:09:26 +0200

    starting commit

--- cart-pole.py
+++ cart-pole.py
@@ -1,11 +1,15 @@
-# import gym
+import gym
 import tensorflow as tf
 
 class Model():
     def __init__(self):
         self.model = tf.keras.Sequential()
 
-env = gym.irgendwas('cart-pole-v0')
+env = gym.make('CartPole-v0')
+env.reset()
 
+# TODO FIRST ONLY EVALUATE AT THE END
+# https://gym.openai.com/docs/
 while True:
-
+    env.render()
+    env.step(env.action_space.sample())
